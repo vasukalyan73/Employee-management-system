c3f6fc96e1e0fee3e4440027524fecaf922e97ba
pavan kalyan v <kalyanvengalasetty73@gmail.com>
2026-03-29 11:44:58 +0530

    Fix production settings: DEBUG=False, ALLOWED_HOSTS for PythonAnywhere

--- emp_details/settings.py
+++ emp_details/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-here-change-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='vasukalyan73.pythonanywhere.com,www.vasukalyan73.pythonanywhere.com', cast=Csv())
 
 
 # Application definition
